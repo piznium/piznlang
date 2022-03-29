1022c20b636fbb9def6c3e615114e10d1f124146
AnpangCreator <59360711+AnpangCreator@users.noreply.github.com>
2022-03-29 10:43:04 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+// Module imports
+#include <stdio.h>
+
+// Other file imports
+
+// Variables
+
+// Constants
+const char superLines[] = "-------------------------------------";
+
+// Run one line of code
+
+// Run multiple lines of code
+void runPiznCode() {
+
+}
+
+// Int main
+int main() {
+    printf("%s\n|           Pizn Compiler           |\n%s\n\n...\n\n",superLines,superLines);
+    //runPiznCode();
+    printf("\n\n...\n\n\n(Done)");
+    return 0;
+}
